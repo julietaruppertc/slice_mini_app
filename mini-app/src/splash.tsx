aa1554be214c37f4cda70a55dfc19a7ea0ae8c98
julietaruppertc <julietaruppertc@gmail.com>
2025-11-20 10:59:29 -0300

    Actualización de splash

--- mini-app/src/splash.tsx
+++ mini-app/src/splash.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect } from 'react';
-import { BG_PURPLE } from './types';
+import { BG_PURPLE } from './types'; // Asume que BG_PURPLE es el color morado
 import type { Screen } from './types';
 
 // Import del SVG guardado en assets
-import lemonSlice from "../assets/svg/lemon_slice.svg";
+import lemonSlice from "../assets/svg/lemon_slice.svg"; // El SVG del limón
+
+// const COLOR_PURPLE = '#806cf2'; 
+// const COLOR_YELLOW = '#f0ee00'; // Color amarillo dorado
 
 const SplashScreen: React.FC<{ navigate: (screen: Screen) => void }> = ({ navigate }) => {
 
     useEffect(() => {
+
         const timer = setTimeout(() => {
             navigate('inicio');
         }, 2000);
@@ -19,21 +23,23 @@ const SplashScreen: React.FC<{ navigate: (screen: Screen) => void }> = ({ naviga
         <div
             style={{
                 width: "100vw",
-                height: "100dvh",   // altura real del dispositivo
-                backgroundColor: BG_PURPLE,
+                height: "100dvh", 
+                backgroundColor: BG_PURPLE, 
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "center",
             }}
         >
+            
             <img
                 src={lemonSlice}
-                alt="lemon logo"
+                alt="Logo Rodaja de Limón"
                 style={{
-                    width: "28vw",
-                    height: "28vw",
-                    maxWidth: 150,
-                    maxHeight: 150,
+                    width: "40vw", 
+                    height: "40vw",
+                    maxWidth: 200, 
+                    maxHeight: 200,
+                 
                 }}
             />
         </div>
